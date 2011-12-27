b555f33cbb39dd2bb1ac023ef9117223b6322b54
Kyle <kyle@nonpolynomial.com>
2011-12-27 13:39:08 -0800

    Removed wallpaper requirements since we no longer have the Wallpaper package available

--- galaxys2.mk
+++ galaxys2.mk
@@ -51,8 +51,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
 	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml 
 
 # The OpenGL ES API level that is natively supported by this device.
 # This is a 16.16 fixed point number
